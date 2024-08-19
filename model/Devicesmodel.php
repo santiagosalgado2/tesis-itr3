45bf1f39093cc5749b707146b2886758be39c324
Santiago Salgado <santiagosalgado@alumnos.itr3.edu.ar>
2024-08-19 18:57:42 -0300

    cambios en los modelos

--- model/Devicesmodel.php
+++ model/Devicesmodel.php
@@ -1,4 +1,37 @@
 <?php
 class Devicesmodel extends Mainmodel{
-    
+    public function getDevicesbyEsp32($id){
+        $query="SELECT
+        d.nombre
+        FROM dispositivos d 
+        JOIN control_dispositivos cd ON cd.ID_dispositivo=d.ID_dispositivo
+        JOIN esp32 e ON e.ID_esp32=cd.ID_esp32
+        WHERE e.ID_esp32=$id";
+        
+        return parent::getDatos($query);
+    }
+
+    public function getDevicesbyUserandEsp32($id,$esp32){
+        $query="SELECT
+        d.nombre
+        FROM dispositivos d 
+        JOIN control_dispositivos cd ON cd.ID_dispositivo=d.ID_dispositivo
+        JOIN esp32 e ON e.ID_esp32=cd.ID_esp32
+        JOIN acceso_usuarios au ON au.ID_control=cd.ID_control
+        JOIN usuarios u ON u.ID_usuario=au.ID_usuario
+        WHERE e.ID_esp32=$esp32 AND u.ID_usuario=$id";
+
+        return parent::getDatos($query);
+    }
+
+    public function getDevice($id){
+        $query="SELECT * FROM Dispositivos WHERE ID_dispositivo=$id LIMIT 1";
+        return parent::getRegistro($query);
+    }
+
+    public function insertDevice($nombre,$tipo){
+        $query="INSERT INTO `dispositivos`(`nombre`, `ID_tipo`) VALUES 
+        ($nombre,$tipo)";
+        $this->db->query($query);
+    }
 }
--- model/Esp32model.php
+++ model/Esp32model.php
@@ -1,4 +1,29 @@
 <?php
 class Esp32model extends Mainmodel{
-    
+    public function getEsp32byUser($id){
+        $query="SELECT 
+        e.ubicacion
+        FROM esp32 e 
+        JOIN control_dispositivos cd ON cd.ID_esp32=e.ID_esp32
+        JOIN acceso_usuarios au ON au.ID_control=cd.ID_control
+        JOIN usuarios u ON u.ID_usuario=au.ID_usuario
+        WHERE u.ID_usuario=$id
+        GROUP BY e.ID_esp32";
+
+        return parent::getDatos($query);
+    }
+
+    public function getEsp32($id){
+        $query="SELECT * FROM esp32 WHERE ID_esp32=$id LIMIT 1";
+
+        return parent::getRegistro($query);
+    }
+
+    public function insertEsp32($mac,$ip,$ubicacion,$estado){
+        $query="INSERT INTO `esp32`( `direccion_mac`, `direccion_ip`, `ubicacion`, `estado`) VALUES 
+        ($mac,$ip,$ubicacion,$estado)";
+
+        $this->db->query($query);
+    }
+
 }
