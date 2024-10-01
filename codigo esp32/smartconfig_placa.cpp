4be8ceec9e6eb1d8cd65eaf459505e27760a4395
santiagosalgado2 <santiagosalgado2007@gmail.com>
2024-10-01 20:27:48 -0300

    qq

---
+++ codigo esp32/smartconfig_placa.cpp
@@ -0,0 +1,25 @@
+#include <WiFi.h>
+#include <esp_smartconfig.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP_STA);
+  Serial.println("Iniciando SmartConfig...");
+  WiFi.beginSmartConfig();
+
+  // Espera hasta que SmartConfig esté completado
+  while (!WiFi.smartConfigDone()) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("SmartConfig completado.");
+  Serial.printf("Conectado a %s\n", WiFi.SSID().c_str());
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Aquí puedes añadir el código que necesites ejecutar continuamente
+}
