65893e6ae0c9dad43b6fe0a97d3ceaa180d94e82
santiagosalgado <santiagosalgado2007@gmail.com>
2024-10-04 20:15:45 -0300

    df

--- codigo esp32/ultimocodigo.cpp
+++ codigo esp32/ultimocodigo.cpp
@@ -3,28 +3,12 @@
 #include <esp_smartconfig.h>
 #include <HTTPClient.h>
 
-const char* serverName = "http://192.168.1.105/pagina_web/pagina_web/public/new_esp/receive";
+const char* serverName = "http://192.168.2.151/pagina_web/pagina_web/public/new_esp/receive";
 //REEMPLAZAR POR LA IP DE LA PC HASTA QUE SE HOSTEE LA PAGINA
-String readMacAddress(){
-  uint8_t baseMac[6];
-  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
-  if (ret == ESP_OK) {
-    char macStr[18];
-    snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-         baseMac[0], baseMac[1], baseMac[2],
-         baseMac[3], baseMac[4], baseMac[5]);
-
-    return macStr;
-  } else {
-    Serial.println("Failed to read MAC address");
-  }
-}
+String code = "8lIsgR9J";
 
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.STA.begin();
-  String macAddress=readMacAddress();
   WiFi.mode(WIFI_AP_STA);
   Serial.println("Iniciando SmartConfig...");
   WiFi.beginSmartConfig();
@@ -52,10 +36,10 @@ void setup() {
     
     
     // Envía la solicitud HTTP GET
-    String serverPath = String(serverName) + "?macAddress=" + macAddress + "&ipAddress=" + espIp;
-    http.begin(serverPath);
+    String serverPath = "code=" + code + "&ipAddress=" + espIp;
+    http.begin(serverName);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpResponseCode = http.GET();
+    int httpResponseCode = http.POST(serverPath);
 
     
 
@@ -73,14 +57,3 @@ void setup() {
 void loop() {
   // Aquí puedes añadir el código que necesites ejecutar continuamente
 }
-
-
-
-
-
-
-
-
-
-
-
