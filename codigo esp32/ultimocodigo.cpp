6e94f991599817e4f5dc8755f0174f4966673ae9
santiagosalgado <santiagosalgado@alumnos.itr3.edu.ar>
2024-10-07 10:56:10 -0300

    reconexion y verificacion de ip funcionando en ultimocodigo.cpp

--- codigo esp32/ultimocodigo.cpp
+++ codigo esp32/ultimocodigo.cpp
@@ -3,57 +3,76 @@
 #include <esp_smartconfig.h>
 #include <HTTPClient.h>
 
-const char* serverName = "http://192.168.2.151/pagina_web/pagina_web/public/new_esp/receive";
-//REEMPLAZAR POR LA IP DE LA PC HASTA QUE SE HOSTEE LA PAGINA
+const char* serverName = "http://192.168.1.105/pagina_web/pagina_web/public/new_esp/receive";
+// REEMPLAZAR POR LA IP DE LA PC HASTA QUE SE HOSTEE LA PAGINA
 String code = "8lIsgR9J";
 
 void setup() {
   Serial.begin(115200);
+  
+  // Intentar conectar automáticamente a la última red conocida
   WiFi.mode(WIFI_AP_STA);
-  Serial.println("Iniciando SmartConfig...");
-  WiFi.beginSmartConfig();
+  WiFi.begin();
 
-  // Espera hasta que SmartConfig esté completado
-  while (!WiFi.smartConfigDone()) {
+  int retries = 0;
+  Serial.println("Intentando conectar a la última red conocida...");
+
+  // Esperar hasta que se conecte o se agoten los intentos de reconexión
+  while (WiFi.status() != WL_CONNECTED && retries < 10) {
+    retries++;
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("SmartConfig completado.");
-  Serial.printf("Conectado a %s\n", WiFi.SSID().c_str());
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-
- String espIp = WiFi.localIP().toString();
-
   if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConectado a la red WiFi.");
+    Serial.printf("Conectado a %s\n", WiFi.SSID().c_str());
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+  } else {
+    Serial.println("\nNo se pudo conectar a la última red. Iniciando SmartConfig...");
+
+    // Si no se puede conectar, inicia SmartConfig
+    WiFi.beginSmartConfig();
+
+    // Espera hasta que SmartConfig esté completado
+    while (!WiFi.smartConfigDone()) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("\nSmartConfig completado.");
+    Serial.printf("Conectado a %s\n", WiFi.SSID().c_str());
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // Si la conexión WiFi está establecida, envía la IP al servidor
+  if (WiFi.status() == WL_CONNECTED) {
+    String espIp = WiFi.localIP().toString();
+    
     // Crea un cliente HTTP
     HTTPClient http;
     
-    // Establece la URL del servidor
-
-    
-    
-    // Envía la solicitud HTTP GET
+    // Envía la solicitud HTTP POST
     String serverPath = "code=" + code + "&ipAddress=" + espIp;
     http.begin(serverName);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
     int httpResponseCode = http.POST(serverPath);
 
-    
-
-        if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println(httpResponseCode);
-            Serial.println(response);
-        } else {
-            Serial.print("Error en la petición: ");
-            Serial.println(httpResponseCode);
-        }
-        http.end();
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error en la petición: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
   }
 }
+
 void loop() {
   // Aquí puedes añadir el código que necesites ejecutar continuamente
 }
